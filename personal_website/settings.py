182c18616a202e6f416481bf675ca5610aaa3066
Abdullah Elsayed <abdullah.fathy@outlook.com>
2020-06-28 18:55:11 +0100

    statics

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+s+c&ky6_gxv)xvz=izu0^#0rpfg_o1&9rim+ek4mjbgrf27%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'onlyabdullah.pythonanywhere.com']
 
